5bf0349e241bc247189d1941faccff32fc197b78
leandro.tavares <leandro.tavares@original.com.br>
2022-02-21 21:11:45 -0300

    cria componente Summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import styled from 'styled-components';
-import {Header} from './components/Header';
-
-const Title = styled.h1`
-  color: red;
-`
+import { Header } from './components/Header';
+import { Dashboard } from './components/Dashboard';
 
 export function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Dashboard />
+    </>
   );
 }
