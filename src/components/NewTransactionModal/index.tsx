f8f9ef21d39aa2cf0060528a112ab16600216a60
leandro.tavares <leandro.tavares@original.com.br>
2022-02-21 22:45:45 -0300

    estiliza formulario do modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import closeImg from '../../assets/close.svg';
 import { Container } from './styles';
 
 type NewTransactionModalOpen = {
@@ -15,7 +16,11 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
+      
       <Container>
+        <button className="react-modal-close" type="button" onClick={onRequestClose}>
+          <img src={closeImg} alt="Fechar modal" />
+        </button>
         <h2>Cadastrar transação</h2>
 
         <input
