63494c23d5984181160a9efb1020f8ac2f0678a6
leandro.tavares <leandro.tavares@original.com.br>
2022-02-21 22:13:36 -0300

    adiciona lib (react-modal) adiona funçoes ao botao do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-export function Header() {
+type HeaderProps = {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({onOpenNewTransactionModal}: HeaderProps) {
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dtmoney" />
-        <button type="button">Nova transação</button>
+        <button
+          type="button"
+          onClick={onOpenNewTransactionModal}
+        >
+          Nova transação
+        </button>
+
       </Content>
     </Container>
   )
