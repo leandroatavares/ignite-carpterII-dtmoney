6bcea0715d74819e80be0cd9fcd37bbf542d76cc
leandro.tavares <leandro.tavares@original.com.br>
2022-02-21 22:49:30 -0300

    lint adjust

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,9 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { api } from '../../services/api';
 import { Container } from "./styles";
 
 export function TransactionsTable() {
-
   useEffect(() => {
     api.get('/transactions')
       .then(response => console.log(response.data))
